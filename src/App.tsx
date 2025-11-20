afbb1dbcf9c0040c3188c61fa187d81f2ed23130
Satyajit Sahoo <satyajit.happy@gmail.com>
2025-11-20 19:22:05 +0100

    Manually navigate to deep link after login

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,15 @@
 import {
+  createPathConfigForStaticNavigation,
   createStaticNavigation,
+  getActionFromState,
+  getStateFromPath,
   StaticParamList,
+  useNavigationContainerRef,
 } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import { createURL } from 'expo-linking';
+import { useEffect, useState } from 'react';
+import { Linking } from 'react-native';
 import { Home, Profile, SignIn, useAuth } from './screens';
 
 const RootStack = createNativeStackNavigator({
@@ -35,15 +41,93 @@ const RootStack = createNativeStackNavigator({
 
 const Navigation = createStaticNavigation(RootStack);
 
+const linking = {
+  enabled: 'auto' as const,
+  prefixes: [createURL('/')],
+};
+
+// Get the deep link configuration for our screens
+// This is used by React Navigation to parse the incoming URLs internally
+// But since we are handling deep links manually, we need this for parsing
+const linkingPathConfig = createPathConfigForStaticNavigation(
+  RootStack,
+  // Pass `linking.options` if present
+  undefined,
+  true,
+);
+
 export function App() {
-  return (
-    <Navigation
-      linking={{
-        enabled: 'auto',
-        prefixes: [createURL('/')],
-      }}
-    />
-  );
+  const ref = useNavigationContainerRef();
+  const authenticated = useAuth((state) => state.authenticated);
+
+  const lastDeepLink = useLastDeepLink();
+
+  // Handle the stored deep link after logging in
+  useEffect(() => {
+    const handleDeepLink = async () => {
+      const url =
+        typeof lastDeepLink === 'string' ? lastDeepLink : await lastDeepLink;
+
+      if (ref.isReady() && url && linkingPathConfig) {
+        // Strip the prefix from the URL
+        const path = linking.prefixes.reduce((acc, prefix) => {
+          if (acc.startsWith(prefix)) {
+            return acc.slice(prefix.length);
+          }
+
+          return acc;
+        }, url);
+
+        // Get the navigation state from the path
+        const state = getStateFromPath(path, { screens: linkingPathConfig });
+
+        // If we have a valid state, get the action and dispatch it
+        // This will navigate to the intended screen
+        if (state) {
+          const action = getActionFromState(state);
+
+          ref.dispatch(action!);
+        }
+      }
+    };
+
+    if (authenticated && lastDeepLink) {
+      handleDeepLink();
+    }
+  }, [authenticated, ref]);
+
+  return <Navigation ref={ref} linking={linking} />;
+}
+
+function useLastDeepLink() {
+  const authenticated = useAuth((state) => state.authenticated);
+
+  // Keep track of the incoming deep links to handle after logging in
+  const [deepLink, setDeepLink] = useState<
+    string | null | Promise<string | null>
+  >(() => (authenticated ? null : Linking.getInitialURL()));
+
+  useEffect(() => {
+    // Clear initial URL after logging in
+    // This prevents handling the same deep link again after logging out and logging back in
+    if (authenticated) {
+      setDeepLink(null);
+    }
+
+    // Store incoming deep links if not authenticated
+    // They will be handled after logging in
+    const subscription = Linking.addEventListener('url', ({ url }) => {
+      if (!authenticated) {
+        setDeepLink(url);
+      }
+    });
+
+    return () => {
+      subscription.remove();
+    };
+  }, [authenticated]);
+
+  return deepLink;
 }
 
 type RootStackParamList = StaticParamList<typeof RootStack>;
